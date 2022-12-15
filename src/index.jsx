80144125a5a00ad865444260833169f5f3edd799
andriwah <andriiwah@gmail.com>
2022-12-15 21:58:42 +0700

    add cra

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SearchCar from './pages/SearchCar';
+import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/car-search" element={<SearchCar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
